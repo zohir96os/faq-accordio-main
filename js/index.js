dca6d58eb65e8e7e8749a9aaf0c985a79745b303
zohiros <zohir96os@gmail.com>
2024-01-05 14:11:58 +0100

    updated js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 //Get elements
 let btns = document.querySelectorAll(".container h3 i");
 let description = document.querySelectorAll(".container p");
-let titles = document.querySelectorAll("h3");
 
 btns.forEach((btn, i) => {
   btn.addEventListener("click", function (e) {
